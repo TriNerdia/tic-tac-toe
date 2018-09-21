a82c27a3fe130783fb0b751b70a1c4076f543f5e
Max Mitschke <maxmitschke@fastmail.com>
2018-09-21 13:11:59 -0400

    Fixing bug buttons can be pressed when HUD visible

--- src/board/Board.gd
+++ src/board/Board.gd
@@ -30,6 +30,7 @@ func _ready():
 		button.connect("pressed", self, "button_pressed", [button])
 
 func reset_board():
+	get_tree().paused = false
 	for i in $Buttons.get_child_count():
 		$Buttons.get_child(i).text = ""
 		
@@ -75,7 +76,9 @@ func button_pressed(button):
 		# the reason the previous controller is returned is that
 		# by the time the code reaches this point, the controller 
 		# of the board will have already been changed.
+		get_tree().paused = true
 		return emit_signal("board_matched", _prev_controller)
 		
 	elif _check_board_for_tie():
+		get_tree().paused = true
 		return emit_signal("board_tied")
